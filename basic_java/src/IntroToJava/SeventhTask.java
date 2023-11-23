ba0e95696843b00e39959b8cb56adbea745e0385
o-zolo <box.zolotar@gmail.com>
2023-11-23 23:40:56 +0200

    Java_part2 tasks.

--- basic_java/src/IntroToJava/SeventhTask.java
+++ basic_java/src/IntroToJava/SeventhTask.java
@@ -1,3 +1,5 @@
+package IntroToJava;
+
 import java.util.Scanner;
 
 import static java.lang.Math.round;
--- basic_java/src/Main.java
+++ basic_java/src/Main.java
@@ -1,7 +1,16 @@
-import java.math.BigInteger;
+import JavaPartTwo.CollectionAnalysis;
+import JavaPartTwo.Rectangle;
 
 public class Main{
+
+    public static String test;
+    public static int some;
     public static void main(String[] args) {
-        SeventhTask.seventhTaskRun();
+//        Rectangle rec = new Rectangle();
+//        rec.calculateArea(10.0, 7.0);
+//        rec.calculatePerimeter(10.0, 7.0);
+//        rec.getDiagonal(10.0, 7.0);
+        CollectionAnalysis col = new CollectionAnalysis();
+        col.listHandler();
     }
 }
